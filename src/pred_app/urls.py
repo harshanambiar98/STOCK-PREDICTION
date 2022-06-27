94220ad0e0b0f7924df028e03c0d6005a1d6380a
nidhuna.n <nidhunanik@gmail.com>
2022-06-27 21:35:59 +0530

    code updated

--- src/pred_app/urls.py
+++ src/pred_app/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
     path('index', views.index, name='index'),
+    path('login/', views.login_view, name="login"),
+    path('register/', views.register_user, name="register"),
+    path("logout/", views.logout_view, name="logout"),
     path('pred', views.pred, name='pred'),
     path('contact', views.contact, name='contact'),
-   ]
+]
